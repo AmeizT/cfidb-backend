fa6d80425fa456c69430467a0d1ce9eb4e12103b
@Tz#1689%gt <zhuwao.tawa@gmail.com>
2023-11-17 10:24:02 +0200

    revert back settings

--- apps/posts/utils.py
+++ apps/posts/utils.py
@@ -1,9 +1,9 @@
 def post_images_path(instance, filename):
     return 'cfidb/posts/{0}/{1}'.format(
-        instance.post.author.username, 
+        instance.branch.name, 
         filename
     )
 
 
 def post_image_url(instance, filename):
-    return 'posts/{0}/{1}'.format(instance.post.author.username, filename)
+    return 'posts/{0}/{1}'.format(instance.branch.name, filename)
