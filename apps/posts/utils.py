36b193d2ad7cf5bf452e1b6f76d9b2b81d3c1e35
@Tz#1689%gt <zhuwao.tawa@gmail.com>
2023-11-18 11:20:35 +0200

    updated the post image save path

--- apps/posts/utils.py
+++ apps/posts/utils.py
@@ -1,9 +1,9 @@
 def post_images_path(instance, filename):
     return 'cfidb/posts/{0}/{1}'.format(
-        instance.branch.name, 
+        instance.post.branch.name, 
         filename
     )
 
 
 def post_image_url(instance, filename):
-    return 'posts/{0}/{1}'.format(instance.branch.name, filename)
+    return 'posts/{0}/{1}'.format(instance.post.branch.name, filename)
