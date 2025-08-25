b8f479b69ce7522c4b5437932d937156c6bdcab0
@Tz#1689%gt <zhuwao.tawa@gmail.com>
2025-08-25 22:59:49 +0200

    Added is_active to the Church model

--- cfidb/settings.py
+++ cfidb/settings.py
@@ -118,7 +118,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cfidb.wsgi.application'
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
